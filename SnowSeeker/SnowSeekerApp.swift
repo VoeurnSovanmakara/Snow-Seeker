b6347ffcb8c26659e5a18a833bc18f377061d9e4
itsshinra <voeurnsovanmakara313@gmail.com>
2026-06-19 14:50:55 +0700

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by sovanmakara on 19/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
